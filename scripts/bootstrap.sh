2e9f9ce8f174c10d331daaa2ff920d4a3acda3bd
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2017-07-03 22:00:36 +0800

    update mysql sql_mode
    Signed-off-by: Ivan Clint A. Pabelona <ipabelona@gmail.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,11 +11,19 @@ sudo cat /vagrant/stubs/httpd.conf.stub > /etc/httpd/conf/httpd.conf
 sudo rm -f /etc/php.ini
 sudo cat /vagrant/stubs/php.ini.stub > /etc/php.ini
 
+# Create sites container
 sudo rm -rf /etc/httpd/sites-available
 sudo rm -rf /etc/httpd/sites-enabled
 sudo mkdir /etc/httpd/sites-available
 sudo mkdir /etc/httpd/sites-enabled
 
+# Set mysql config
+sudo rm -f /etc/my.cnf
+sudo cp /vagrant/stubs/my.cnf /etc/my.cnf
+sudo service mysqld restart
+
+
+# Initialize settings
 sudo php /vagrant/adzbuzz_init
 
 # Start httpd
